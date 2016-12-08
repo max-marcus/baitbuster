1e43e813c9d01a97e44df0b8608f31256edf0d40
listentomm <maxmarcususc@gmail.com>
2016-12-07 22:31:04 -0800

    jquery mouse enter and leave for target links

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const clickbait = ["http://drudgereport.com.co/", "http://abcnews.com.co/", "http://politicops.com/", "http://worldnewsdailyreport.com/"];
 const satire = ["http://www.theonion.com/"]
 
@@ -5,15 +7,23 @@ function clipUrl(url) {
   let count = 0, index = null;
 
   while (count < 3 && index !== -1) {
-    index = url.indexOf('/', index+1);
-    count++;
+    index = url.indexOf('/', index + 1);
+    count += 1;
   }
-  return 
+  return url.slice(0, index + 1);
 }
 
-$('a').mouseover(() => {
-  const url = $(this).attr('href');
-  if (clickbait.indexOf(url) !== -1) {
-    $(this).toggleClass('classname')
+$(document).on({
+  mouseenter: function() {
+    let url = $(this).attr('href');
+    url = clipUrl(url);
+    if (clickbait.indexOf(url) !== -1) {
+      console.log('moused over target link: ', $(this));
+      $(this).after('<div class="bb">This is where the box goes</div>');
+    }
+  },
+  mouseleave: function() {
+    $(this).next('.bb').remove();
   }
-})
+}, 'a');
+
